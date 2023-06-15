67992bb201154b356ef25786010a8a1a4fc6cfd8
chris9618 <chrisjerry9618@gmail.com>
2023-06-15 13:07:02 +0530

    index.js statement change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 8081;
 
-app.get('/', (req, res) => res.send('Welcome to NodeJs EC2 Deployment Session!'));
+app.get('/', (req, res) => res.send('Welcome to NodeJs Application Site!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
